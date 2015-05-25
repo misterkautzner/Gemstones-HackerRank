91afb8012b7f263a1af29a45771dcc6688b11521
John Kautzner <misterkautzner@gmail.com>
2015-05-25 16:16:31 -0500

    second commit

--- src/GemStones.java
+++ src/GemStones.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
+//This is a comment I'm using to create a second version on github
 public class GemStones {
 
 	public static void main(String args[]) {
